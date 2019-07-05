9ebc268beb91aeb017d66e3cf5b83a85f793fd1e
David Vávra <david@vavra.me>
2019-07-05 15:20:56 +0200

    Title and tabs

--- app/src/app/app.component.spec.ts
+++ app/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'player'`, () => {
+  it(`should have as title 'app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('player');
+    expect(app.title).toEqual('app');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to player!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
   });
 });
