75c2efd434224b5e4d7261d2fab00b97c1efcc2a
David Vávra <david@vavra.me>
2019-07-06 20:23:48 +0200

    Feedback

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -41,16 +41,16 @@ export class AppComponent {
   ]
   primaryDoneActions: Action[] = [
     { description: "Sabotáž íránského jaderného programu", df: 12, visibility: 'covert', type: 'main', delegate: "Daniel Appleby", keyword: "OHEŇ V DÍŘE", result: "Naše obrovská a mezinárodně koordinovaná akce nepřinesla žádané ovoce. Povedlo se nám sice vyhodit do vzduchu kompletní zařízení Íránu - ale už vesměs prázdné a navíc za cenu rozsáhlé zpravodajské akce, kterou asi dokáže někdo zpětně dořešit.", targetDelegation: "flag-ir" },
-    { description: "Afghánistán - elektrifikace měst", df: 5, visibility: 'public', type: 'main', delegate: "Harold Cross", keyword: "", result: "Proběhlo, Afghánistán se připojuje k modernímu světu.", targetDelegation: "flag-af" },
+    { description: "Podpora integrace Palestinských uprchlíků v Sýrii", df: 2, visibility: 'public', type: 'main', delegate: "Daniel Appleby", keyword: "", result: "Současná situace v Sýrii je na hraně občanské války, tudíž se není příliš kam integrovat. Ale můžete ovlivnit, na čí stranu se přidají.", targetDelegation: "flag-sy" },
     { description: "Ovlivňování veřejného mínění ohledně budoucí federace Izraele a Palestiny", df: 5, visibility: 'public', type: 'main', delegate: "Daniel Appleby", keyword: "FEDERACE", result: "Propagační kampaně ze strany Britů vyvolávají odpor Izraelců.", targetDelegation: "flag-il" },
-    { description: "Inspekce vodní elektrárny mezi Indií a Pakistánem", df: 2, visibility: 'public', type: 'main', delegate: "Harold Cross", keyword: "CHANEB", result: "Vše je v pořádku, stavba ja na dobré cestě.", targetDelegation: "flag-in" },
-    { description: "Podpora integrace Palestinských uprchlíků v Sýrii", df: 2, visibility: 'public', type: 'main', delegate: "Daniel Appleby", keyword: "", result: "Současná situace v Sýrii je na hraně občanské války, tudíž se není příliš kam integrovat. Ale můžete ovlivnit, na čí stranu se přidají.", targetDelegation: "flag-sy" }
+    { description: "Afghánistán - elektrifikace měst", df: 5, visibility: 'public', type: 'main', delegate: "Harold Cross", keyword: "", result: "Proběhlo, Afghánistán se připojuje k modernímu světu.", targetDelegation: "flag-af" },
+    { description: "Inspekce vodní elektrárny mezi Indií a Pakistánem", df: 2, visibility: 'public', type: 'main', delegate: "Harold Cross", keyword: "CHANEB", result: "Vše je v pořádku, stavba ja na dobré cestě.", targetDelegation: "flag-in" }
   ]
   secondaryDoneActions: Action[] = [
-    { description: "Výstavba přístavu v Gaze", df: 5, visibility: 'public', type: 'support', delegate: "Harold Cross", keyword: "PŘÍSTAV", result: "Vybudováno", targetDelegation: "flag-ps" },
     { description: 'Kampaň v UK: "Proč mít euro je super"', df: 10, visibility: 'public', type: 'support', delegate: "Daniel Appleby", keyword: "EURO", result: "Britové jsou nakloněni přechodu na Euro.", targetDelegation: "gb" },
     { description: "Sleva na dodání stíhaček Eurofigter do SA", df: 0, visibility: 'public', type: 'other', delegate: "Daniel Appleby", keyword: "", result: "OK", targetDelegation: "flag-sa" },
-    { description: "Zakázka od firmy BAS v oboru tězké strojírenství", df: 0, visibility: 'public', type: 'economic', delegate: "Daniel Appleby", keyword: "", result: "Smlouvy byly uzavřeny", targetDelegation: "flag-sa" }
+    { description: "Zakázka od firmy BAS v oboru tězké strojírenství", df: 0, visibility: 'public', type: 'economic', delegate: "Harold Cross", keyword: "", result: "Smlouvy byly uzavřeny", targetDelegation: "flag-sa" },
+    { description: "Výstavba přístavu v Gaze", df: 5, visibility: 'public', type: 'support', delegate: "Harold Cross", keyword: "PŘÍSTAV", result: "Vybudováno", targetDelegation: "flag-ps" }
   ]
   sent = false
   availableDf = 60
