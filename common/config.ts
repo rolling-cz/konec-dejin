3d8937e2b85992319ca326aec5059c9b0071f314
David Vávra <david@vavra.me>
2019-11-10 19:43:20 +0100

    Proper countries

--- common/config.ts
+++ common/config.ts
@@ -1,14 +1,30 @@
 // https://afeld.github.io/emoji-css/
 export const COUNTRIES: ValueName[] = [
     { value: 'flag-af', name: 'Afgánistán' },
+    { value: 'flag-am', name: 'Arménie' },
+    { value: 'flag-az', name: 'Ázerbajdžán' },
+    { value: 'flag-cn', name: 'Čína' },
+    { value: 'flag-eg', name: 'Egypt' },
+    { value: 'flag-fr', name: 'Francie' },
     { value: 'flag-in', name: 'Indie' },
     { value: 'flag-iq', name: 'Irák' },
     { value: 'flag-ir', name: 'Írán' },
+    { value: 'flag-it', name: 'Itálie' },
     { value: 'flag-il', name: 'Izrael' },
+    { value: 'flag-jo', name: 'Jordánsko' },
+    { value: 'flag-lb', name: 'Libanon' },
+    { value: 'flag-ly', name: 'Libye' },
+    { value: 'flag-de', name: 'Německo' },
+    { value: 'flag-pk', name: 'Pákistán' },
     { value: 'flag-ps', name: 'Palestina' },
+    { value: 'flag-gr', name: 'Řecko' },
+    { value: 'flag-ru', name: 'Rusko' },
     { value: 'flag-sa', name: 'Saudská Arábie' },
     { value: 'flag-sy', name: 'Sýrie' },
-    { value: 'gb', name: 'Velká Británie' }
+    { value: 'flag-tr', name: 'Turecko' },
+    { value: 'flag-us', name: 'USA' },
+    { value: 'gb', name: 'Velká Británie' },
+    { value: 'question', name: 'Jiné' }
 ]
 
 export const ACTION_TYPES: ValueName[] = [
