8a6ffaec2c6dbcb65ea44e3af0b279ca8ccfdd6c
David Vávra <david@vavra.me>
2020-12-28 23:14:02 +0100

    Kopírování akcí, BV a titulků do dalších kol, update functions dependencies (closes #9)

--- functions/src/database.ts
+++ functions/src/database.ts
@@ -42,6 +42,11 @@ export async function doProcessMainActionsChange(delegateId: string, roundId: st
             await admin.database().ref("actions/" + roundId + "/" + lastActionId).remove()
         }
     }
+    // Change mainActions also for the next round of the same size
+    let nextRoundId = await getNextRoundIdSameSize(roundId);
+    if (nextRoundId != null) {
+        await admin.database().ref("delegateRounds/" + delegateId + "/" + nextRoundId + "/mainActions").set(mainActions)
+    }
 }
 
 export async function doProcessDelegationChange(delegateId: string, roundId: string, change: Change<DataSnapshot>) {
@@ -61,4 +66,94 @@ export async function doProcessDelegationChange(delegateId: string, roundId: str
     actionIds.forEach(async actionId => {
         await admin.database().ref("actions/" + roundId + "/" + actionId + "/delegation").set(delegationId)
     })
+    // Change delegation also for the next round
+    let nextRoundId = await getNextRoundId(roundId);
+    if (nextRoundId != null) {
+        await admin.database().ref("delegateRounds/" + delegateId + "/" + nextRoundId + "/delegation").set(delegationId)
+    }
+}
+
+export async function doProcessBvChange(delegateId: string, roundId: string, change: Change<DataSnapshot>) {
+    // Exit when the data is deleted.
+    if (!change.after.exists()) {
+        return;
+    }
+    const bv = change.after.val() as number;
+    if (bv == null) {
+        return;
+    }
+    // Change BV also for the next round of the same size
+    let nextRoundId = await getNextRoundIdSameSize(roundId);
+    if (nextRoundId != null) {
+        await admin.database().ref("delegateRounds/" + delegateId + "/" + nextRoundId + "/bv").set(bv)
+    }
+}
+
+export async function doProcessActionTitleChange(roundId: string, actionId: string, change: Change<DataSnapshot>) {
+    // Exit when the data is deleted.
+    if (!change.after.exists()) {
+        return;
+    }
+    const title = change.after.val() as string;
+    if (title == null || title == "") {
+        return;
+    }
+    // Change title also for the next round of the same size
+    let nextRoundId = await getNextRoundIdSameSize(roundId);
+    if (nextRoundId != null) {
+        let actionIdWithSameIndex = await getActionIdWithSameIndexAndDelegate(roundId, nextRoundId, actionId)
+        if (actionIdWithSameIndex != null) {
+            await admin.database().ref("actions/" + nextRoundId + "/" + actionIdWithSameIndex + "/title").set(title)
+        }
+    }
+}
+
+async function getNextRoundId(currentRoundId: string) {
+    var currentFound = false;
+    var nextRoundId = null;
+    (await admin.database().ref("rounds").once("value")).forEach(snap => {
+        if (snap.key == currentRoundId) {
+            currentFound = true
+        } else if (currentFound) {
+            nextRoundId = snap.key
+            currentFound = false
+        }
+    })
+    return nextRoundId
+}
+
+async function getNextRoundIdSameSize(currentRoundId: string) {
+    var currentSize = (await admin.database().ref("rounds/"+currentRoundId+"/size").once("value")).val();
+    var currentFound = false;
+    var nextRoundId = null;
+    (await admin.database().ref("rounds").once("value")).forEach(snap => {
+        if (snap.key == currentRoundId) {
+            currentFound = true
+        } else if (currentFound && snap.val()["size"] == currentSize) {
+            nextRoundId = snap.key
+            currentFound = false
+        }
+    })
+    return nextRoundId
+}
+
+async function getActionIdWithSameIndexAndDelegate(currentRoundId: string, nextRoundId: string, actionId: string) {
+    var delegateId = (await admin.database().ref("actions/"+currentRoundId+"/"+actionId+"/delegate").once("value")).val();
+    var actionIndex = -1;
+    var index = 0;
+    (await admin.database().ref("actions/"+currentRoundId).orderByChild("delegate").equalTo(delegateId).once("value")).forEach(snap => {
+        if (snap.key == actionId) {
+            actionIndex = index;
+        }
+        index++;
+    });
+    index = 0;
+    var actionIdWithSameIndex = null;
+    (await admin.database().ref("actions/"+nextRoundId).orderByChild("delegate").equalTo(delegateId).once("value")).forEach(snap => {
+        if (index == actionIndex) {
+            actionIdWithSameIndex = snap.key
+        }
+        index++;
+    })
+    return actionIdWithSameIndex
 }
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,18 +1,18 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import { doLogin, doSwissLogin } from './login';
-import { doProcessMainActionsChange, doProcessDelegationChange } from './database';
+import { doProcessMainActionsChange, doProcessDelegationChange, doProcessBvChange, doProcessActionTitleChange } from './database';
 
 admin.initializeApp({
     databaseURL: "https://dune-new-dawn.firebaseio.com"
 });
 
 export let login = functions.https.onRequest(async (request, response) => {
-    await doLogin(request.query["password"], response)
+    await doLogin(request.query["password"] as string, response)
 })
 
 export let swissLogin = functions.https.onRequest(async (request, response) => {
-    await doSwissLogin(request.query["password"], response)
+    await doSwissLogin(request.query["password"] as string, response)
 })
 
 export let processMainActionsChange = functions.database.ref("delegateRounds/{delegateId}/{roundId}/mainActions").onWrite(async (change, context) => {
@@ -21,4 +21,12 @@ export let processMainActionsChange = functions.database.ref("delegateRounds/{de
 
 export let processDelegationChange = functions.database.ref("delegateRounds/{delegateId}/{roundId}/delegation").onWrite(async (change, context) => {
     await doProcessDelegationChange(context.params.delegateId, context.params.roundId, change)
+})
+
+export let processBvChange = functions.database.ref("delegateRounds/{delegateId}/{roundId}/bv").onWrite(async (change, context) => {
+    await doProcessBvChange(context.params.delegateId, context.params.roundId, change)
+})
+
+export let processActionTitleChange = functions.database.ref("actions/{roundId}/{actionId}/title").onWrite(async (change, context) => {
+    await doProcessActionTitleChange(context.params.roundId, context.params.actionId, change)
 })
